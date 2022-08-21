05b01e3d1ac894ce185d910138a00fea5238c075
AlexeyZ <zao1988@rambler.ru>
2022-08-21 13:43:02 +0300

    Homework_commit_1

--- hw1/Program.cs
+++ hw1/Program.cs
@@ -1,2 +1,15 @@
 ﻿
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите первое число");
+int a1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите первое число");
+int a2 = int.Parse(Console.ReadLine());
+if (a1>a2) 
+{
+Console.WriteLine ("Первое число больше второго");
+Console.WriteLine (a1 + ">" + a2);
+}
+else 
+{
+Console.Write ("Второе число больше первого");
+Console.WriteLine (a2 + ">" + a1);
+}
