9620b9fbc589ab42cfadfa8bc1903f810520c6ef
AlexeyZ <zao1988@rambler.ru>
2022-08-23 21:36:24 +0300

    3 tasks added

---
+++ hw8/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+Console.WriteLine("Введите число:");
+string a1 = Console.ReadLine();
+int countStr = a1.Length;
+if (countStr < 3)
+{
+Console.WriteLine(">>>>> Третьей цифры нет");
+}
+else
+{
+Console.WriteLine(a1[2]);
+}
