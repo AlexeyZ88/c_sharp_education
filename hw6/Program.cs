4c66aa27688f0f29a3d2f7c9449a92bdf33ddc0e
AlexeyZ <zao1988@rambler.ru>
2022-08-23 18:09:16 +0300

    2 tasks added

---
+++ hw6/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Введите трёхзначное число:");
+int num = int.Parse(Console.ReadLine());
+int ed = num%10;
+int dec = (num/10)%10;
+int hund = num/100;
+Console.WriteLine("\n");
+Console.WriteLine(hund);
+Console.WriteLine(ed);
+Console.WriteLine(dec);
