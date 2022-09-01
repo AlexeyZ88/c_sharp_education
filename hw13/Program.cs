242d6b841ddca74b349efd69f3ab375f12771506
AlexeyZ <zao1988@rambler.ru>
2022-09-01 21:40:47 +0300

    dz 4____2 of 3  tasks completed

---
+++ hw13/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Идёт загрузка программы");
+for (int j = 0; j < 30; j++)
+{
+ System.Threading.Thread.Sleep(50);;
+ Console.Write("||| ");
+}
+Console.WriteLine("\n Поехалиии....");
+Random rn = new Random();
+for (int i = 0; i < 10; i++)
+{
+    int a1 = rn.Next(1, 10);
+    int a2 = rn.Next(1, 10);
+    Console.WriteLine(">>> " + Math.Pow(a1, a2));
+}
+
+
+
+
+
---
+++ hw14/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Начинаем...");
+Random rn = new Random();
+int a1 = rn.Next(10, 9999);
+Console.WriteLine("Наше число:" + a1);
+string a2 = a1.ToString();
+int count = 0;
+char num;
+for (int i = 0; i < a2.Length; i++)
+{
+    num = a2[i];
+    //count = count + (num - '0');
+    count = count + (int)Char.GetNumericValue(num);
+}
+Console.WriteLine("сумма цифр нашего числа = " + count);
