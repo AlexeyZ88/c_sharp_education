b2a2d1d944cb54d95c1905757e8d9d0c3bf94819
AlexeyZ <zao1988@rambler.ru>
2022-09-02 19:07:58 +0300

    dz5_task2_ completed

---
+++ hw17/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+Random rndm1 = new Random();
+int a1 = rndm1.Next(10, 20);
+int[] arr1 = new int[a1];
+Console.WriteLine("Объявлен массив на " + a1 + " элементов");
+
+int count = 0;
+for (int i = 0; i < a1; i++)
+{
+    int a2 = rndm1.Next(100, 999);
+    arr1[i] = a2;
+    Console.WriteLine(i + ")"+ arr1[i]);
+    if (a2 % 2 == 0)
+    {
+        count=count+i;
+    }
+
+}
+Console.WriteLine("Сумма индеков четных элементов массива - " + count);
