8d54d7354499d8846a5f85a970e85bae07da787b
AlexeyZ <zao1988@rambler.ru>
2022-09-07 21:13:17 +0300

    dz_5_task1_completed

---
+++ pr5_1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 4 
+Console.WriteLine("Введите неколько чисел через заптятую:");
+int num = 0;
+int count = 0;
+string a1 = Console.ReadLine();
+if (a1 == string.Empty)
+{
+    Console.WriteLine("Пользователь ничего не ввёл");
+}
+else
+{
+    string[] subs = a1.Split(',');
+    foreach (var sub in subs)
+    {
+        num = int.Parse(sub);
+        //Console.WriteLine($"Substring: {sub}");
+        if (num > 0)
+        {
+            count += 1;
+        }
+    }
+    Console.WriteLine($"Количество положительных чисел: {count}");
+}
+
