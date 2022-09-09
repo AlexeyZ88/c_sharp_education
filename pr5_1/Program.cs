f36e3227a90d133dd3bd858e9b27e7761b43fcaa
AlexeyZ <zao1988@rambler.ru>
2022-09-09 15:32:44 +0300

    pr_5_2 tasks_completed

--- pr5_1/Program.cs
+++ pr5_1/Program.cs
@@ -1,26 +1,36 @@
-﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 //0, 7, 8, -2, -2 -> 2
 //1, -7, 567, 89, 223-> 4 
-Console.WriteLine("Введите неколько чисел через заптятую:");
 int num = 0;
 int count = 0;
+Console.WriteLine("Введите неколько чисел через запятую:");
+Char[] ArrayOfSep = { ',', '.', ':', ';' };
 string a1 = Console.ReadLine();
+
 if (a1 == string.Empty)
 {
     Console.WriteLine("Пользователь ничего не ввёл");
 }
 else
 {
-    string[] subs = a1.Split(',');
+    foreach (char item in ArrayOfSep)
+    {
+        a1 = a1.Replace(item, '|');
+    }
+string[] subs = a1.Split('|');
+
     foreach (var sub in subs)
     {
+        //string temp;
+        //temp= String.Concat(sub.Where(Char.IsDigit));
         num = int.Parse(sub);
-        //Console.WriteLine($"Substring: {sub}");
+        Console.WriteLine(num);
         if (num > 0)
         {
             count += 1;
         }
     }
-    Console.WriteLine($"Количество положительных чисел: {count}");
 }
-
+Console.WriteLine($"Количество положительных чисел: {count}"); 
---
+++ pr5_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.WriteLine("Введите b1");
+int b1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите k1");
+int k1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите b2");
+int b2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите k2");
+int k2 = int.Parse(Console.ReadLine());
+double x1=0;
+double y1=0;
+if (k1==k2) 
+{
+    Console.WriteLine("Прямые параллельны и не имеют точки пересечения. введите другие аргументы");
+}
+else 
+{
+    x1 =(double)(b2-b1)/(k1-k2);
+    y1 = k1*x1+b1;
+    Console.WriteLine ($"Точка пересечения прямых имеет координаты x={x1}, y={y1}");
+}
+
