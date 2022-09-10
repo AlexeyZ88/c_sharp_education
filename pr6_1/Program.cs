a95594c33a961b43d66032a2a39fe756f291c073
AlexeyZ <zao1988@rambler.ru>
2022-09-10 20:20:52 +0300

    pr6_task1,2 completed

---
+++ pr6_1/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+ */
+
+Console.WriteLine("Начинаем");
+int m = 5; //строки
+int n = 6; //столбцы
+double[,] NewArray = new double[m, n];
+
+double[,] FillArray(double [,] ArrayToFill)
+{
+    
+    for (int i = 0; i < ArrayToFill.GetLength(0); i++)
+    {
+        for (int j = 0; j < ArrayToFill.GetLength(1); j++)
+        {
+            ArrayToFill[i, j] =Math.Round(new Random().NextDouble()*20,1) ;
+        }
+        
+    }
+    return ArrayToFill;
+}
+
+
+
+
+void PrintArray (double[,] ArrToPrint)
+{
+    for (int i = 0; i < ArrToPrint.GetLength(0); i++)
+    {
+      for (int j = 0; j < ArrToPrint.GetLength(1); j++)
+      {
+        Console.Write($"{ArrToPrint[i,j]} ");
+      } 
+      Console.WriteLine(); 
+    }
+}
+FillArray(NewArray);
+PrintArray(NewArray);
---
+++ pr6_2/Program.cs
@@ -0,0 +1,51 @@
+﻿/* Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет */
+
+Console.WriteLine("Начинаем");
+int m = 7; //строки
+int n = 7; //столбцы
+int[,] NewArray = new int[m, n];
+
+int [,] FillArray(int [,] ArrayToFill)
+{
+    
+    for (int i = 0; i < ArrayToFill.GetLength(0); i++)
+    {
+        for (int j = 0; j < ArrayToFill.GetLength(1); j++)
+        {
+            ArrayToFill[i, j] = new Random().Next(1,30) ;
+        }
+        
+    }
+    return ArrayToFill;
+}
+
+
+FillArray(NewArray);
+Console.WriteLine("Введите целое двухзначное число:");
+string Temp = Console.ReadLine();
+
+int ItemCount = Temp.Length;
+if (ItemCount !=2) 
+{
+    Console.WriteLine("Введено неверное число!");
+}
+else
+{
+ int RowNum = Temp [0] - '0';
+ int ColNum = Temp [1] - '0';
+ try
+ {
+    Console.WriteLine ($"Элемент по адресу {RowNum}, {ColNum} = {NewArray[RowNum-1, ColNum-1]}");
+ }
+ catch (System.Exception)
+ {
+   Console.WriteLine ($"Элемент массива по адресу {RowNum},{ColNum} отсутствует"); 
+ } 
+}
+
