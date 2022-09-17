d016406929a690d1003801e10b98f25450d05416
AlexeyZ <zao1988@rambler.ru>
2022-09-17 07:49:30 +0300

    pr9_Task1

---
+++ pr9_1/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4, 6, 7, 8"
+ */
+
+ int M = new Random().Next(1,10);
+ int N = new Random().Next (11,30);
+string ResultStr = String.Empty;
+ Console.WriteLine($"Число М={M}");
+ Console.WriteLine($"Число N={N}");
+ for (int i = M; i < N; i++)
+ {
+  int Temp = i;
+ResultStr += Temp.ToString() + ", ";
+ 
+ }
+ 
+ Console.WriteLine($"{ResultStr}{N}"); 
+
+
