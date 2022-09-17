8570f85c42d0afe2c95968a16889297c6599e9de
AlexeyZ <zao1988@rambler.ru>
2022-09-17 13:43:28 +0300

    pr9_1TASK1_fixed

--- pr9_1/Program.cs
+++ pr9_1/Program.cs
@@ -3,7 +3,7 @@ M = 1; N = 5. -> ""1, 2, 3, 4, 5""
 M = 4; N = 8. -> ""4, 6, 7, 8"
  */
 
- int M = new Random().Next(1,10);
+/*  int M = new Random().Next(1,10);
  int N = new Random().Next (11,30);
 string ResultStr = String.Empty;
  Console.WriteLine($"Число М={M}");
@@ -15,6 +15,25 @@ ResultStr += Temp.ToString() + ", ";
  
  }
  
- Console.WriteLine($"{ResultStr}{N}"); 
+ Console.WriteLine($"{ResultStr}{N}");  */
 
 
+int M = new Random().Next(1, 10);
+int N = new Random().Next(11, 30);
+string ResultStr = String.Empty;
+string PrintStr(int ArgM, int ArgN, string ResultString)
+{
+if  (ArgM<ArgN)
+{
+ResultString +=ArgM.ToString() + ",";   
+ResultString = PrintStr(ArgM+1,ArgN,ResultString);
+return ResultString;
+}
+else {
+return ResultString;
+}
+}
+ResultStr = PrintStr(M, N, ResultStr);
+Console.WriteLine($"Число М={M}");
+Console.WriteLine($"Число N={N}");
+Console.WriteLine($"{ResultStr}{N}");
