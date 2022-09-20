7b6474a99617954c3995c7707466a7cc95dbd37d
AlexeyZ <zao1988@rambler.ru>
2022-09-20 19:12:21 +0300

    pr9_3 tasks completed

--- pr9_2/Program.cs
+++ pr9_2/Program.cs
@@ -2,7 +2,7 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30 */
 
- int M = new Random().Next(1,10);
+/*  int M = new Random().Next(1,10);
  int N = new Random().Next (11,30);
 int ResultSum = 0;
  Console.WriteLine($"Число М={M}");
@@ -11,5 +11,28 @@ int ResultSum = 0;
  {
   ResultSum += i;
  }
- Console.WriteLine($"M={M}, N={N} =>> {ResultSum}");
- 
+ Console.WriteLine($"M={M}, N={N} =>> {ResultSum}"); */
+
+
+int M = new Random().Next(1, 6);
+int N = new Random().Next(7, 10);
+int ResultSum = 0;
+
+int Sum(int ArgM, int ArgN, int ResultSum1)
+{
+    if (ArgM <= ArgN)
+    {
+        ResultSum1 = ResultSum1 + ArgM;
+        ResultSum1 = Sum(ArgM + 1, ArgN, ResultSum1);
+        return ResultSum1;
+    }
+    else
+    {
+        return ResultSum1;
+    }
+}
+ResultSum = Sum(M, N, ResultSum);
+Console.WriteLine($"Число М={M}");
+Console.WriteLine($"Число N={N}");
+Console.WriteLine($"Сумма целых чисел от {M} до {N} = {ResultSum}");
+
---
+++ pr9_3/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9 */
+
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+//int Akker = 0;
+ int Akk(int n, int m)
+{
+    if (n == 0) return m + 1;
+    if (n != 0 && m == 0) return Akk(n - 1, 1);
+    if (n > 0 && m > 0) return Akk(n - 1, Akk(n, m - 1));
+    return Akk(n, m);
+}
+if (m<0)
+{
+Console.WriteLine ("Введено неверное число M. Введите целое положительное число");
+}
+else if (n<0)
+{
+  Console.WriteLine ("Введено неверное число N. Введите целое положительное число");  
+}
+else
+{
+  Akk(n, m);  
+}
+int Akker = Akk(m,n);
+Console.WriteLine (Akker);
+
